5abec528934a370d99f150ce0c51fe891b165559
KARA PEDERSEN <KJPED74@GMAIL.COM>
2021-08-04 10:34:50 -0700

    all test passing I think

--- src/containers/RickAndMortyCharacters.test.jsx
+++ src/containers/RickAndMortyCharacters.test.jsx
@@ -4,13 +4,13 @@ import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 import RickAndMortyCharacters from './RickAndMortyCharacters';
 
-// const server = setupServer(
+const server = setupServer(
 //   rest.get(
 
-//     'https://rickandmortyapi.com/api/character',
-//     (req, res, ctx) => {
-//       return res(
-//         ctx.json(
+  //     'https://rickandmortyapi.com/api/character',
+  //     (req, res, ctx) => {
+  //       return res(
+  //         ctx.json(
 
 //           [...Array(+req.params.count)].map((_, i) => ({
 //             character: 'Rick',
@@ -21,15 +21,16 @@ import RickAndMortyCharacters from './RickAndMortyCharacters';
 //       );
 //     }
 //   )
-// );
+);
 
 describe('RickAndMortyCharacters container', () => {
-  // beforeAll(() => server.listen());
-  // afterAll(() => server.close());
+  beforeAll(() => server.listen());
+  afterAll(() => server.close());
 
   it('displays a list of characters', async () => {
 
     render(<RickAndMortyCharacters />);
+ 
 
     screen.getByText('Loading...');
 
